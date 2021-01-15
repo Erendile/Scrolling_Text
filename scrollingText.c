6682446c1d628eb6579bda06c36fd09a75251359
Erendile <74563063+Erendile@users.noreply.github.com>
2021-01-15 20:16:08 +0300

    Create scrollingText.c

---
+++ scrollingText.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
+    char text[] = "SCROLLING TEXT";
+    int b = strlen(text);
+    for(int i = 0; i <= b; i++)
+    {
+        for(int j = b - i; j < b; j++)
+            printf("%c", text[j]);
+        system("cls");
+    }
+    for(int i = 0; i < b; i++)
+        printf("%c",text[i]);
