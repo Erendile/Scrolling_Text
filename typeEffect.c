39e00c03a54487e6b975e39a4af25b351d3f27c7
Erendile <74563063+Erendile@users.noreply.github.com>
2021-01-19 09:35:40 +0300

    Create typeEffect.c

---
+++ typeEffect.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    char text[] = "I COULDN'T FIND A NAME";
+    int s = strlen(text);
+    for(int i = 0; i < s; i++)
+    {
+        for(int j = 0; j <= i; j++)
+        {
+            printf("%c",text[j]);
+        }
+        if(i != s - 1)
+            system("cls");
+    }
+
+    return 0;
+}
